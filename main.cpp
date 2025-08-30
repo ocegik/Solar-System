f7da73d7021bb58b7c9bcdc9ea96a437d9e0df22
ocegik <147722344+ocegik@users.noreply.github.com>
2025-08-30 11:12:32 +0530

    Added Texture Files

--- main.cpp
+++ main.cpp
@@ -3,9 +3,22 @@
 #include "constants.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SMFL");
-    sf::CircleShape shape(50.f);
-        shape.setFillColor(sf::Color(100, 250, 50));
+    sf::RenderWindow window(sf::VideoMode({1600, 1000}), "SMFL");
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("Textures/8k_stars.jpg")) {
+        return -1;
+    }
+    sf::Sprite backgroundSprite(backgroundTexture);
+    sf::CircleShape sun(Constants::SunRadius * Constants::SCALE_RADIUS);
+    sun.setFillColor(Constants::SunColor);
+    sun.setOrigin({sun.getRadius(), sun.getRadius()});
+    sun.setPosition({600, 400}); // center of screen
+
+    sf::CircleShape earth(Constants::EarthRadius * Constants::SCALE_RADIUS);
+    earth.setFillColor(Constants::EarthColor);
+    earth.setOrigin({earth.getRadius(), earth.getRadius()});
+    earth.setPosition({600 + Constants::EarthOrbit * Constants::SCALE_DISTANCE, 400});
+
     
     while (window.isOpen()) {
         while (auto event = window.pollEvent()) {
@@ -14,7 +27,9 @@ int main() {
         }
         
         window.clear();
-        window.draw(shape); 
+        window.draw(backgroundSprite);
+        window.draw(sun);
+        window.draw(earth);
         window.display();
     }
 }
